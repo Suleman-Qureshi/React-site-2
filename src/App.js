3b78461f641e37950bdc7fe11d33c92f6eb78958
DEMN9999 <demn729999@gmail.com>
2024-03-01 15:16:32 +0500

    mobile menu added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import MobileMeun from './component/MobileMeun';
 function App() {
   return (
     <>
-    <MobileMeun/>
+    <span className='absolute top-2 right-2 z-40'>
+      <MobileMeun/>
+    </span>
     <div className='mainImage w-screen h-screen overflow-hidden flex justify-center items-center flex-col'>
     <span className='text-white text-sm tracking-widest absolute right-1 bottom-1'><p>Develped & Designed By <b className='text-green-500'>DesignSpecturm</b></p></span>
     <Outlet/>
